50f0cd722c7e5a1622ed28e80d9b84f00c82fabd
sri Hasini <gayathrii2507@gmail.com>
2024-11-13 00:01:36 +0530

    added initial Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import srikar from "../Assests/srikar.jpeg";
+import cv from "../Assests/cv.pdf";
 
 const Home = () => {
-    return (
+  return (
+    <div className="HomePage">
+      <div className="text-section">
+        <div>Hi, I am Sreekar</div>
+        <div> FrontEnd Developer</div>
         <div>
-            <h1>Hi, This is Sreekar</h1>
-            <img src="" alt="Sreekar" />
-            <h2>Tech Stack</h2> <p>React, JavaScript, CSS, etc.</p> 
-            <h2>Achievements</h2> <p>Your achievements...</p> 
-            <h2>Hobbies</h2> 
-            <p>Your hobbies...</p>
+          Full Stack Developer with 3+ years of experience in building
+          responsive and scalable web applications using modern frameworks such
+          as Vue.js, React, and Node.js. Proficient in TypeScript and JavaScript
+          for frontend development and experienced with Python for backend
+          services. Adept in Agile methodologies, DevOps practices, and
+          cloud-based deployments with tools like Heroku and GitHub Actions.
         </div>
-    )
-}
+        <a href={cv} className="download-button">
+          My Resume
+          <div className="tooltip">
+            <p>Sreekar Dathi</p>
+            <p>Full Stack Developer</p> <p>Hyderabad</p>
+            <p>Email: sreekarnagasai@gmail.com</p> <p>Phone: 8008629639</p>
+          </div>
+        </a>
+      </div>
+      <img src={srikar} alt="Srikar" />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
